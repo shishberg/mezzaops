1eadd316137ef470b5bbda1500eca6eb7008e5b7
David McLeish <dave@mcleish.id.au>
2023-02-26 14:06:35 +1100

    subcommands

--- main.go
+++ main.go
@@ -18,6 +18,22 @@ var (
 	guildID   = flag.String("guild-id", "", "Guild ID, or empty to register globally")
 )
 
+func subCommand(name, desc string) *discordgo.ApplicationCommandOption {
+	return &discordgo.ApplicationCommandOption{
+		Name:        name,
+		Description: desc,
+		Type:        discordgo.ApplicationCommandOptionSubCommand,
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "task",
+				Description: "Task name",
+				Type:        discordgo.ApplicationCommandOptionString,
+				Required:    true,
+			},
+		},
+	}
+}
+
 var (
 	defaultMemberPermissions int64 = discordgo.PermissionManageServer
 
@@ -27,26 +43,10 @@ var (
 			Description: "MezzaOps",
 			Type:        discordgo.ChatApplicationCommand,
 			Options: []*discordgo.ApplicationCommandOption{
-				{
-					Name:        "start",
-					Description: "Start",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
-				},
-				{
-					Name:        "stop",
-					Description: "Stop",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
-				},
-				{
-					Name:        "restart",
-					Description: "Restart",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
-				},
-				{
-					Name:        "logs",
-					Description: "Logs",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
-				},
+				subCommand("start", "Start"),
+				subCommand("stop", "Stop"),
+				subCommand("restart", "Restart"),
+				subCommand("logs", "Logs"),
 			},
 		},
 	}
@@ -73,13 +73,19 @@ func main() {
 		case "ops":
 			options := i.ApplicationCommandData().Options
 			msg := "ops"
+			var task string
 			if len(options) != 0 {
 				msg = options[0].Name
+				for _, opt := range options[0].Options {
+					if opt.Name == "task" {
+						task = opt.StringValue()
+					}
+				}
 			}
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: fmt.Sprintf("Hello %s!", msg),
+					Content: fmt.Sprintf("Hello %s %s!", msg, task),
 				},
 			})
 		}
@@ -98,6 +104,10 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+
+	for _, cmd := range cmds {
+		session.ApplicationCommandDelete(session.State.User.ID, *guildID, cmd.ID)
+	}
 
 	log.Println("Shutting down.")
 }
