d0f60a142b01270cfa61bc56efaf1bb825208994
David McLeish <dave@mcleish.id.au>
2023-02-26 13:43:00 +1100

    slash command

--- main.go
+++ main.go
@@ -14,6 +14,19 @@ import (
 
 var (
 	tokenFile = flag.String("token", "token.txt", "file containing the bot token")
+	guildID   = flag.String("guild-id", "", "Guild ID, or empty to register globally")
+)
+
+var (
+	defaultMemberPermissions int64 = discordgo.PermissionManageServer
+
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "ops",
+			Description: "MezzaOps",
+			Type:        discordgo.ChatApplicationCommand,
+		},
+	}
 )
 
 func main() {
@@ -31,6 +44,28 @@ func main() {
 		log.Fatal(err)
 	}
 	defer session.Close()
+
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.ApplicationCommandData().Name {
+		case "ops":
+			log.Println("hello")
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hello ops!",
+				},
+			})
+		}
+	})
+
+	var cmds []*discordgo.ApplicationCommand
+	for _, c := range commands {
+		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, *guildID, c)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cmds = append(cmds, cmd)
+	}
 
 	log.Println("Running.")
 	sc := make(chan os.Signal, 1)
